32d48d442b294887add623db40e9a57e048226e4
Brian Kopp <briankopp.usa@gmail.com>
2019-09-16 22:32:08 -0600

    Setup Package (#1)

---
+++ src/url-devaluer.ts
@@ -0,0 +1,105 @@
+import * as regexps from './reg-expressions';
+
+/**
+ * Class which strips values out of URLs.
+ * Usage:
+ * const urlDevaluer = new UrlDevaluer();
+ * urlDevaluer.devalueUrl('https://example.com/hello/123');
+ * // https://github.com/hello/:intId
+ */
+export class UrlDevaluer {
+    private intReplacementName: string;
+    private floatReplacementName: string;
+    private guidReplacementName: string;
+    private extraTemplatePatterns: {
+        [key: string]: RegExp
+    };
+
+    /**
+     * Creates a new UrlDevaluer with options
+     * @param {Object} options - Options for the UrlDevaluer
+     * @param {string} options.intReplacementName - The value to replace integer values, default=intId
+     * @param {string} options.floatReplacementName - The value to replace float values, default=value
+     * @param {string} options.guidReplacementName - The value to replace guid values, default guidId
+     * @param {Object} options.extraTemplatePatterns - Dictionary of extra template patterns to run
+     * on the URL. Formatted like { key: StringOrRegExp }
+     */
+    constructor(
+        options: {
+            intReplacementName?: string,
+            floatReplacementName?: string,
+            guidReplacementName?: string,
+            extraTemplatePatterns?: {
+                [key: string]: string | RegExp
+            }
+        } = {}
+    ) {
+        this.intReplacementName = options.intReplacementName || 'intId';
+        this.floatReplacementName = options.floatReplacementName || 'value';
+        this.guidReplacementName = options.guidReplacementName || 'guidId';
+        this.extraTemplatePatterns = {};
+        if (options.extraTemplatePatterns) {
+            for (const name of Object.keys(options.extraTemplatePatterns)) {
+                const value = options.extraTemplatePatterns[name];
+                if (typeof value === 'string') {
+                    this.extraTemplatePatterns[name] = RegExp(value);
+                } else if (value instanceof RegExp) {
+                    this.extraTemplatePatterns[name] = value;
+                } else {
+                    throw new Error(`Unexpected type ${typeof value} in options.extraTemplatePatterns. Expected string | RegExp`);
+                }
+            }
+        }
+    }
+
+    /**
+     * Strips values out of a URL
+     * @param {string} url url, with or without host.
+     * @returns {string} the URL with values stripped out of it
+     */
+    public devalueUrl(url: string): string {
+        const urlParts = url.split('?');
+        if (urlParts.length === 0) {
+            return url;
+        }
+        const routePart = urlParts[0];
+        const routeParts = routePart.split('/');
+        const templatedParts: string[] = [];
+
+        for (const part of routeParts) {
+            let newPart = part;
+            if (this.isInteger(part)) {
+                newPart = `:${this.intReplacementName}`;
+            } else if (this.isGuid(part)) {
+                newPart = `:${this.guidReplacementName}`;
+            } else if (this.isFloat(part)) {
+                newPart = `:${this.floatReplacementName}`;
+            } else {
+                for (const name in Object.keys(this.extraTemplatePatterns)) {
+                    if (this.extraTemplatePatterns[name].test(part)) {
+                        newPart = `:${name}`;
+                        break;
+                    }
+                }
+            }
+            templatedParts.push(newPart);
+        }
+
+        return templatedParts.join('/');
+    }
+    private isInteger(part: string): boolean {
+        return regexps.intRegex.test(part);
+    }
+    private isFloat(part: string): boolean {
+        return regexps.floatRegex.test(part);
+    }
+    private isGuid(part: string): boolean {
+        for (const r of regexps.guidRegexes) {
+            if (r.test(part)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/url-devaluer.spec.js
@@ -0,0 +1,105 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const expect = chai.expect;
+const UrlDevaluer = require('../src/url-devaluer').UrlDevaluer;
+
+
+describe('UrlDevaluer class unit tests', () => {
+    it('should accept an empty constructor and default arguments', () => {
+        const urlDevaluer = new UrlDevaluer();
+        expect(urlDevaluer.intReplacementName).to.eql('intId');
+        expect(urlDevaluer.floatReplacementName).to.eql('value');
+        expect(urlDevaluer.guidReplacementName).to.eql('guidId');
+        expect(Object.keys(urlDevaluer.extraTemplatePatterns).length).to.eql(0);
+    });
+    it('should accept overwrites to replacement names', () => {
+        const urlDevaluer = new UrlDevaluer({
+            intReplacementName: 'intReplace',
+            guidReplacementName: 'guidReplace',
+            floatReplacementName: 'floatReplace'
+        });
+        expect(urlDevaluer.intReplacementName).to.eql('intReplace');
+        expect(urlDevaluer.floatReplacementName).to.eql('floatReplace');
+        expect(urlDevaluer.guidReplacementName).to.eql('guidReplace');
+    });
+    it('should accept extra template patterns in either string or RegExp', () => {
+        const urlDevaluer = new UrlDevaluer({
+            extraTemplatePatterns: {
+                'string': 'abc',
+                'regexp': RegExp('abc')
+            }
+        });
+        const keys = Object.keys(urlDevaluer.extraTemplatePatterns);
+        expect(keys).to.eql(['string', 'regexp']);
+        expect(urlDevaluer.extraTemplatePatterns).to.eql({
+            'string': RegExp('abc'),
+            'regexp': RegExp('abc')
+        });
+    });
+    it('should error when extra template patterns not regex or string', () => {
+        try {
+            const urlDevaluer = new UrlDevaluer({
+                extraTemplatePatterns: {
+                    'notAString': 5
+                }
+            });
+            expect.fail();
+        } catch (err) {
+            expect(err.message).to.eql('Unexpected type number in options.extraTemplatePatterns. Expected string | RegExp');
+        }
+    });
+    it('should strip integer out of url', () => {
+        const url = 'https://example.com/123';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:intId');
+    });
+    it('should strip hyphenated, lowercase guid out of url', () => {
+        const url = 'https://example.com/8304e058-b5dd-477c-9891-bb8b36faa28e';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:guidId');
+    });
+    it('should strip hyphenated, uppercase guid out of url', () => {
+        const url = 'https://example.com/947059EF-08BB-4570-8C3A-CC41B384BF10';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:guidId');
+    });
+    it('should strip unhyphenated, lowercase guid out of url', () => {
+        const url = 'https://example.com/b3a4eb83bb8a427f891f8aff1863c66e';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:guidId');
+    });
+    it('should strip unhyphenated, uppercase guid out of url', () => {
+        const url = 'https://example.com/E7FC5E4326BB4434970C77A66A5FCA88';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:guidId');
+    });
+    it('should not strip value when too short to be a guid', () => {
+        const url = 'https://example.com/FC5E4326BB4434970C77A66A5FCA88';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/FC5E4326BB4434970C77A66A5FCA88');
+    });
+    it('should not strip value when too long to be a guid', () => {
+        const url = 'https://example.com/E7FC5E4326BB4434970C77A66A5FCA88A8';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/E7FC5E4326BB4434970C77A66A5FCA88A8');
+    });
+    it('should strip floating point value out of url', () => {
+        const url = 'https://example.com/1.2';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('https://example.com/:value');
+    });
+    it('should replace int ids when only the route is passed', () => {
+        const url = '/hello/123';
+        const urlDevaluer = new UrlDevaluer();
+        const result = urlDevaluer.devalueUrl(url);
+        expect(result).to.eql('/hello/:intId');
+    });
+});
